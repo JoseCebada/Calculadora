8ccffd122ea29b627a82a2577f7029c55a0d2d2b
Alumno <Alumno@PC27>
2019-08-28 12:35:57 -0500

    Conversor grados Celsius - Fahrenheit

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Conversor
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbC = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbC
+            // 
+            this.lbC.AutoSize = true;
+            this.lbC.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbC.Location = new System.Drawing.Point(33, 52);
+            this.lbC.Name = "lbC";
+            this.lbC.Size = new System.Drawing.Size(172, 22);
+            this.lbC.TabIndex = 0;
+            this.lbC.Text = "Grados Centígrados";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Grados Fahrenheit";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(229, 52);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(170, 20);
+            this.txtC.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(229, 125);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(170, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.BackColor = System.Drawing.Color.Black;
+            this.btnConvertir.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvertir.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnConvertir.Location = new System.Drawing.Point(304, 163);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(95, 31);
+            this.btnConvertir.TabIndex = 4;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.UseVisualStyleBackColor = false;
+            this.btnConvertir.Click += new System.EventHandler(this.BtnConvertir_Click);
+            // 
+            // Conversor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 206);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbC);
+            this.Name = "Conversor";
+            this.Text = "Conversor de Temperaturas";
+            this.Load += new System.EventHandler(this.Conversor_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbC;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnConvertir;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Conversor : Form
+    {
+        int bandera = 0;
+        public Conversor()
+        {
+            InitializeComponent();
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtC_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //txtC.Text = "";
+            //textBox2.Text = "32.0";
+            bandera = 1;
+            if (e.KeyChar == 13)
+            {
+                BtnConvertir_Click(sender, e);
+            }
+        }
+
+        private void TextBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //txtC.Text = "0.0";
+            //textBox2.Text = "";
+            bandera = 2;
+            if (e.KeyChar == 13)
+            {
+                BtnConvertir_Click(sender, e);
+            }
+        }
+
+        private void Conversor_Load(object sender, EventArgs e)
+        {
+            btnConvertir.Focus();
+        }
+
+        private void BtnConvertir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double grados;
+                //Si se escribio en centigrados
+                if (bandera == 1)
+                {
+                    grados = Convert.ToDouble(txtC.Text) * 9 / 5.0 + 32.0;
+                    //Mostramos el resultado
+                    textBox2.Text = string.Format("{0:F2}", grados);
+                }
+                if (bandera == 2)
+                {
+                    grados = (Convert.ToDouble(txtC.Text) - 32) * 5.0 / 9.0;
+                    //Mostramos el resultado
+                    txtC.Text = string.Format("{0:F2}", grados);
+                }
+            }
+            catch(FormatException)
+            {
+                txtC.Text = "0.0";
+                textBox2.Text = "32.00";
+            }
+        }
+
+    }
+}
